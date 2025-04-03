c7141a75e64bbadd0824188e32a30bcbcaf25ec6
[Niggle Nandu] <nigglenandu@gmail.com>
2025-04-03 19:47:35 +0545

    project

---
+++ src/main/java/nigglenandu/foodigo/foodigo/FoodigoApplication.java
@@ -0,0 +1,13 @@
+package nigglenandu.foodigo.foodigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodigoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodigoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nigglenandu/foodigo/foodigo/FoodigoApplicationTests.java
@@ -0,0 +1,13 @@
+package nigglenandu.foodigo.foodigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
