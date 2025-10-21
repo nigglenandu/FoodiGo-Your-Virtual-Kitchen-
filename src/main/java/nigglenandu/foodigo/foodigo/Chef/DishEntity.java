a934e6554af5b60db3e1c1a2e7a14aee7ce61213
[Niggle Nandu] <nigglenandu@gmail.com>
2025-10-21 23:38:01 +0545

    dish entity

---
+++ src/main/java/nigglenandu/foodigo/foodigo/Chef/DishEntity.java
@@ -0,0 +1,35 @@
+package nigglenandu.foodigo.foodigo.Chef;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+public class DishEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long dishId;
+
+    private String dishName;
+    private String description;
+    private double price;
+    private boolean available;
+
+    // Link this dish to a specific Chef
+    @ManyToOne
+    @JoinColumn(name = "chef_id")
+    private ChefEntity chef;
+
+    public DishEntity() {}
+
+    public DishEntity(String dishName, String description, double price, boolean available, ChefEntity chef) {
+        this.dishName = dishName;
+        this.description = description;
+        this.price = price;
+        this.available = available;
+        this.chef = chef;
+    }
+}
