741620377922fdab2cc9285340078d1af3c2c9b0
[Niggle Nandu] <nigglenandu@gmail.com>
2025-10-24 23:30:50 +0545

    dish repo

---
+++ src/main/java/nigglenandu/foodigo/foodigo/Chef/DishRepository.java
@@ -0,0 +1,8 @@
+package nigglenandu.foodigo.foodigo.Chef;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface DishRepository extends JpaRepository<DishEntity, Long> {
+    List<DishEntity> findByChefChefId(Long chefId);
+}
